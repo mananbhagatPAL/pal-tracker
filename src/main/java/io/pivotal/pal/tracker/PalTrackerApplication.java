66b9a85bfb60b1dea3595b5840d805b42d0d46be
mananbhagatPAL <manan.bhagat@perficient.com>
2019-05-31 11:37:58 -0400

    updated travis with dependency

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -21,9 +21,9 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    TimeEntryRepository timeEntryRepository() {
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+//        MysqlDataSource dataSource = new MysqlDataSource();
+//        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
         return new JdbcTimeEntryRepository(dataSource);
     }
 
